f8dcbffe55352559cf8c8fce0cbc2731b166ec26
Rocco Pagliarulo <roccopagliarulo364@gmail.com>
2026-08-31 19:25:56 +0200

    Increase JMH sample size for reliable confidence intervals Warmup 5 iter, measurement 10 iter, 3 fork

--- src/test/java/org/apache/commons/collections4/benchmark/PatriciaTrieBenchmark.java
+++ src/test/java/org/apache/commons/collections4/benchmark/PatriciaTrieBenchmark.java
@@ -42,9 +42,9 @@ import org.openjdk.jmh.annotations.Warmup;
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Thread)
-@Warmup(iterations = 2, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
-@Fork(1)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(3)
 public class PatriciaTrieBenchmark {
 
     private static final int CAPACITY = 1000;
